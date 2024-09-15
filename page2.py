cdb171d070b68ac48c43a00cf725f4409a26498f
tareklichtenfeld <110313535+tareklichtenfeld@users.noreply.github.com>
2024-09-15 17:07:59 +0200

    Add files via upload

--- page2.py
+++ page2.py
@@ -1,5 +1,28 @@
 import streamlit as st
+from datetime import time
 
 st.title("find your groupride")
-st.write("'find your groupride' ist eine interaktive Möglichkeit, deinen perfekten groupride in Münster und im Münsterland zu finden. Beantworte einfach ein paar Fragen zu dir und wie du am liebsten Fahrrad fährst, und wir finden gemeinsam die perfekte Gruppe für dich")
+st.write("'find your groupride' ist eine interaktive Möglichkeit, deinen perfekten groupride in Münster und im Münsterland zu finden. Beantworte einfach ein paar Fragen zu dir und wie du am liebsten Fahrrad fährst, und wir finden gemeinsam die perfekte Gruppe für dich.")
+
+st.markdown("")
+st.markdown("")
+
+st.header("Infos über dich")
+
+pace = st.slider("Geschwindigkeit", 20, 40, (25, 32))
+lower_pace, higher_pace = pace
+st.write(f"Ich fahre in der Gruppe gerne eine Geschwindigkeit zwischen {lower_pace} und {higher_pace} km/h.")
+
+st.markdown("")
+st.markdown("")
+st.markdown("")
+
+
+time = st.slider(
+    "Uhrzeit", value=(time(11, 30), time(12, 45))
+)
+
+start_time, end_time=time
+
+st.write(f"Ich habe normalerweise an Wochentagen zwischen {start_time} und {end_time} Uhr Zeit für ne Ausfahrt.")
 
