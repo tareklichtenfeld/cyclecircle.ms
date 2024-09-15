de60b0a07336101c57197629ad68e325def1e13e
tareklichtenfeld <110313535+tareklichtenfeld@users.noreply.github.com>
2024-09-15 16:47:42 +0200

    Add files via upload

--- cyclecircle_ms.py
+++ cyclecircle_ms.py
@@ -8,7 +8,7 @@ hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
+            header {visibility: visible;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
@@ -24,23 +24,15 @@ st.logo("cyclecircle_logo_big.png")
 #-----SIDEBAAARRR----------------------------------------------------------
 st.sidebar.header('cyclecircle.ms')
 
-import streamlit as st
-
-
 page1 = st.Page("page1.py", title="Übersicht", icon=":material/overview_key:")
 page2 = st.Page("page2.py", title="find your groupride", icon=":material/directions_bike:")
 page3 = st.Page("page3.py", title="Über cyclecircle.ms", icon=":material/info:")
 page4 = st.Page("page4.py", title="Q&A", icon=":material/contact_support:")
 page5 = st.Page("page5.py", title="Kontakt", icon=":material/alternate_email:")
 
-pg = st.navigation({
-            "Find a group ride": [
-                page1, page2
-            ],
-            "Infos": [
-                page3, page4, page5
-            ],
+pages = {
+        "For you :D": [page1, page2],
+        "Infos": [page3, page4, page5],
         }
-)
-
+pg = st.navigation(pages)
 pg.run()
